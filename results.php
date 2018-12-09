c42f6192eb4202222bb4e4de76247527a6104ffb
michellewang03 <45047726+michellewang03@users.noreply.github.com>
2018-12-09 09:47:01 -0600

    Update results.php

--- results.php
+++ results.php
@@ -1,29 +1,148 @@
 <?php
-
 include "config.php";
 
-$num = rand(1,3);
-echo"<h1>Your result is: </h1>";
-switch($num){
-	case 1:
-		echo"Nike Brand";
-		break;
-	case 2:
-		echo"Vans Brand";
-		break;
-	case 3:
-		echo"Gucci Brand";
-		break;
-	case 4:
-		echo"Converse Brand";
-		break;
-	case 5:
-		echo"Adidas Brand";
-		break;
-	case 6:
-		echo"Uggs Brand";
-		break;
+$gscore = 0;
+$vscore = 0;
+$uscore = 0;
+$nscore = 0;
+if ($_POST["use"] == "gucci") {
+	$gscore += 1;
+} 
+elseif ($_POST["use"] == "vans"){
+	$vscore += 1;
+}
+elseif ($_POST["use"] == "ugg"){
+	$uscore += 1;
+}
+elseif ($_POST["use"] == "nike"){
+	$nscore += 1;
 }
 
+if ($_POST["mat"] == "gucci") {
+	$gscore += 1;
+} 
+elseif ($_POST["mat"] == "vans"){
+	$vscore += 1;
+}
+elseif ($_POST["mat"] == "ugg"){
+	$uscore += 1;
+}
+elseif ($_POST["mat"] == "nike"){
+	$nscore += 1;
+}
 
+if ($_POST["price"] == "gucci") {
+	$gscore += 1;
+} 
+elseif ($_POST["price"] == "vans"){
+	$vscore += 1;
+}
+elseif ($_POST["price"] == "ugg"){
+	$uscore += 1;
+}
+elseif ($_POST["price"] == "nike"){
+	$nscore += 1;
+}
+
+if ($_POST["type"] == "gucci") {
+	$gscore += 1;
+} 
+elseif ($_POST["type"] == "vans"){
+	$vscore += 1;
+}
+elseif ($_POST["type"] == "ugg"){
+	$uscore += 1;
+}
+elseif ($_POST["type"] == "nike"){
+	$nscore += 1;
+}
+
+if ($_POST["season"] == "gucci") {
+	$gscore += 1;
+} 
+elseif ($_POST["season"] == "vans"){
+	$vscore += 1;
+}
+elseif ($_POST["season"] == "ugg"){
+	$uscore += 1;
+}
+elseif ($_POST["season"] == "nike"){
+	$nscore += 1;
+}
+
+if ($_POST["in"] == "gucci") {
+	$gscore += 1;
+} 
+elseif ($_POST["in"] == "vans"){
+	$vscore += 1;
+}
+elseif ($_POST["in"] == "ugg"){
+	$uscore += 1;
+}
+elseif ($_POST["in"] == "nike"){
+	$nscore += 1;
+}
+
+if ($_POST["char"] == "gucci") {
+	$gscore += 1;
+} 
+elseif ($_POST["char"] == "vans"){
+	$vscore += 1;
+}
+elseif ($_POST["char"] == "ugg"){
+	$uscore += 1;
+}
+elseif ($_POST["char"] == "nike"){
+	$nscore += 1;
+}
+
+if ($_POST["strength"] == "gucci") {
+	$gscore += 1;
+} 
+elseif ($_POST["strength"] == "vans"){
+	$vscore += 1;
+}
+elseif ($_POST["strength"] == "ugg"){
+	$uscore += 1;
+}
+elseif ($_POST["strength"] == "nike"){
+	$nscore += 1;
+}
+
+if ($_POST["weight"] == "gucci") {
+	$gscore += 1;
+} 
+elseif ($_POST["weight"] == "vans"){
+	$vscore += 1;
+}
+elseif ($_POST["weight"] == "ugg"){
+	$uscore += 1;
+}
+elseif ($_POST["weight"] == "nike"){
+	$nscore += 1;
+}
+
+if ($_POST["sole"] == "gucci") {
+	$gscore += 1;
+} 
+elseif ($_POST["sole"] == "vans"){
+	$vscore += 1;
+}
+elseif ($_POST["sole"] == "ugg"){
+	$uscore += 1;
+}
+elseif ($_POST["sole"] == "nike"){
+	$nscore += 1;
+}
+
+$gscore =($gscore / 10) * 100;
+$vscore =($vscore / 10) * 100;
+$uscore =($uscore / 10) * 100;
+$nscore =($nscore / 10) * 100;
+
+echo "Here are your scores for each shoe brand. The brand with the highest score is the brand which is best fit for you!<br>
+Gucci: $gscore%<br>
+Vans: $vscore%<br>
+UGG: $uscore%<br>
+Nike: $nscore%";
 ?>
